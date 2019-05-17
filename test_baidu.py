2be245ab1b1ba4eaba139c01d93deced37a7f665
zheng kai hua <1060627453@qq.com>
2019-05-17 17:17:30 +0800

    第二次更新

--- test_baidu.py
+++ test_baidu.py
@@ -4,26 +4,25 @@ from selenium import webdriver
 import unittest, time
 
 
-# class BaiduTest(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = webdriver.Chrome()
-#         self.driver.implicitly_wait(30)  # 隐性等待时间为30秒
-#         self.base_url = "https://www.baidu.com"
-#
-#     def test_baidu(self):
-#         driver = self.driver
-#         driver.get(self.base_url + "/")
-#         driver.find_element_by_id("kw").clear()
-#         driver.find_element_by_id("kw").send_keys("unittest")
-#         driver.find_element_by_id("su").click()
-#         time.sleep(3)
-#         title = driver.title
-#         self.assertEqual(title, u"unittest_百度搜索")
-#
-#     def tearDown(self):
-#         self.driver.quit()
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
-#这是一份测试文件
+class BaiduTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)  # 隐性等待时间为30秒
+        self.base_url = "https://www.baidu.com"
+
+    def test_baidu(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_id("kw").clear()
+        driver.find_element_by_id("kw").send_keys("unittest")
+        driver.find_element_by_id("su").click()
+        time.sleep(3)
+        title = driver.title
+        self.assertEqual(title, u"unittest_百度搜索")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
